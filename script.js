ca55bf11ea8fa6a39bfe2dc7f24bd1137803442b
Alan <31021805+alanlortiz@users.noreply.github.com>
2026-01-01 22:59:09 -0600

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const startDate = new Date(2025, 4, 0, 12, 0, 0);
+const startDate = new Date(2025, 0, 0, 0, 0, 0);
 
 /* =========================
    VARIABLES GLOBALES
@@ -305,5 +305,6 @@ canvas.addEventListener("touchstart", (e) => {
 }, { passive: false });
 
 animate();
+
 
 
